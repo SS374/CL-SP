55819f8366efec5217118582ea8c8827b9ba18fe
SS374 <tyler.wang374@gmail.com>
2024-01-25 18:26:13 -0500

    formatting

--- climatology.py
+++ climatology.py
@@ -1,9 +1,19 @@
 # Datasets to test out, with their intended prediction
-#datasets = [[37, 37, 37, 38, 38, 38, 38, 37, 37, 37, 36, 36, 36, 37, 38, 40, 42, 44, 45, 45, 44, 43, 42, 41] ] # Schnectady Hourly forecast on 1/24/24 || For forecasting
-#datasets = [[40], [42],[44],[46]] # Example data || For linear prediction || Target of 48
-#datasets = [[31,38],[36,45],[36,43]] # Highs and lows from 1/24/24 to 1/26/24 || For non-linear prediciton || Target of 35,41 
-datasets = [[1], [1], [2], [3], [5], [8], [13], [21]] # FIBONACCI SEQUENCE || For non-linear prediction || Target of 34
-#datasets = [[1],[2],[4],[7]] # LAZY CATERER'S SEQUENCE || For non-linear prediction || Target of 11
+# datasets = [[37, 37, 37, 38, 38, 38, 38, 37, 37, 37, 36, 36, 36, 37, 38, 40, 42, 44, 45, 45, 44, 43, 42, 41] ] # Schnectady Hourly forecast on 1/24/24 || For forecasting
+# datasets = [[40], [42],[44],[46]] # Example data || For linear prediction || Target of 48
+# datasets = [[31,38],[36,45],[36,43]] # Highs and lows from 1/24/24 to 1/26/24 || For non-linear prediciton || Target of 35,41
+datasets = [
+    [1],
+    [1],
+    [2],
+    [3],
+    [5],
+    [8],
+    [13],
+    [21],
+]  # FIBONACCI SEQUENCE || For non-linear prediction || Target of 34
+# datasets = [[1],[2],[4],[7]] # LAZY CATERER'S SEQUENCE || For non-linear prediction || Target of 11
+
 
 # Simple base functions
 def findavg(values):
@@ -12,12 +22,14 @@ def findavg(values):
         total += value
     return total / len(values)
 
+
 def findhigh(values):
     high = values[0]
     for value in values:
         if high < value:
             high = value
     return high
+
 
 def findlow(values):
     low = values[0]
@@ -26,20 +38,22 @@ def findlow(values):
             low = value
     return low
 
+
 def findmedian(values):
     if len(values) % 2 == 0:
         return (values[len(values) // 2] + values[len(values) // 2 - 1]) / 2
     else:
         return values[len(values) // 2]
 
+
 # Finds general dataset trend in multiple datasets
 def findtrend(datasets):
     oldavg = findavg(datasets[0])
-    switches = 0 # Keeps track of when the trend changes
+    switches = 0  # Keeps track of when the trend changes
     trend = None
 
     for dataset in datasets:
-        newavg = findavg(dataset) # Based on the dataset's average
+        newavg = findavg(dataset)  # Based on the dataset's average
         if newavg > oldavg:
             if trend == "Temperature is decreasing":
                 switches += 1
@@ -54,12 +68,11 @@ def findtrend(datasets):
 
         oldavg = newavg
 
-    
-    if switches > 2: # Unable to return trend if trend is inconsitent
+    if switches > 2:  # Unable to return trend if trend is inconsitent
         trend = "Too much variation in temperaure to find a trend"
     elif switches != 0:
         # Finds net trend
-        if findavg(datasets[-1]) > findavg(datasets[0]): 
+        if findavg(datasets[-1]) > findavg(datasets[0]):
             nettrend = "is increasing"
 
         if findavg(datasets[-1]) <= findavg(datasets[0]):
@@ -69,16 +82,21 @@ def findtrend(datasets):
 
     return trend
 
+
 # Predicts next dataset based on current datasets
 # Limitations: Only checks last 3 datasets, no other information other than values provided in datasets, cannot indentify peaks and valleys in temperature
 def predict(datasets):
-    last_dataset = datasets[-1] # Last dataset 
-    secondtl_dataset = datasets[-2] # Second to last dataset
-    thirdtl_dataset = datasets[-3] # Third to last dataset
-    predictionlist = [None for x in range(len(last_dataset))] # Placeholer list for prediction dataset
+    last_dataset = datasets[-1]  # Last dataset
+    secondtl_dataset = datasets[-2]  # Second to last dataset
+    thirdtl_dataset = datasets[-3]  # Third to last dataset
+    predictionlist = [
+        None for x in range(len(last_dataset))
+    ]  # Placeholer list for prediction dataset
     trend = findtrend(datasets)
 
-    if "same" in trend: # If all previous data was identical, assume prediction will be identical
+    if (
+        "same" in trend
+    ):  # If all previous data was identical, assume prediction will be identical
         return (
             "["
             + "".join(str(x) for x in last_dataset)
@@ -87,9 +105,13 @@ def predict(datasets):
         )
 
     elif "increasing" in trend or "decreasing" in trend:
-        deviances = [] # Stores deviances for different index locations
-        weightedPercentages = [] # Stores deviance percentage for different index locations
-        for i in range(len(last_dataset)): # Looks at each index of datasets, only uses values at the same index location to form prediction
+        deviances = []  # Stores deviances for different index locations
+        weightedPercentages = (
+            []
+        )  # Stores deviance percentage for different index locations
+        for i in range(
+            len(last_dataset)
+        ):  # Looks at each index of datasets, only uses values at the same index location to form prediction
             larger = last_dataset[i]
             smaller1 = secondtl_dataset[i]
             smaller2 = thirdtl_dataset[i]
@@ -98,25 +120,33 @@ def predict(datasets):
             factor = larger / smaller1
             factor2 = smaller1 / smaller2
 
-            # Converts factor into a percentage 
-            if factor > 1 and factor2 < 1: 
+            # Converts factor into a percentage
+            if factor > 1 and factor2 < 1:
                 factor = 2 - factor
             elif factor < 1 and factor2 > 1:
                 factor2 = 2 - factor2
 
-            if difference1 == difference2: # Finds if differences between past datasets was linear
-                predictionlist[i] = larger + difference1 # Applies linear difference
+            if (
+                difference1 == difference2
+            ):  # Finds if differences between past datasets was linear
+                predictionlist[i] = larger + difference1  # Applies linear difference
                 deviance = 0
                 weightedPercentage = 0
             else:
-                predictionlist[i] = round(larger * factor) # Applies non-linear difference
-                deviance = abs(round((larger * factor - larger * (factor + factor2)/2))) # Finds predicted potential deviance from the prediction 
-                weightedPercentage = deviance/(larger*factor) # Finds the percentage off the deviance is from the prediction
+                predictionlist[i] = round(
+                    larger * factor
+                )  # Applies non-linear difference
+                deviance = abs(
+                    round((larger * factor - larger * (factor + factor2) / 2))
+                )  # Finds predicted potential deviance from the prediction
+                weightedPercentage = deviance / (
+                    larger * factor
+                )  # Finds the percentage off the deviance is from the prediction
             deviances.append(deviance)
             weightedPercentages.append(weightedPercentage)
     else:
         return "Too much variation to predict"
-    
+
     # Sets deviance and weighted percentages to the highest value for confidence calculation
     deviance = findhigh(deviances)
     weightedPercentages = findhigh(weightedPercentages)
@@ -124,13 +154,33 @@ def predict(datasets):
     # Finds confidence in prediction based on deviance or deviance percentage
     if deviance == 0 or weightedPercentage == 0:
         confidence = "Absolutely Certain"
-    elif deviance > 0 and deviance <= 1 or weightedPercentage > 0 and weightedPercentage <= 10 :
+    elif (
+        deviance > 0
+        and deviance <= 1
+        or weightedPercentage > 0
+        and weightedPercentage <= 10
+    ):
         confidence = "High"
-    elif deviance > 1 and deviance <= 4 or weightedPercentage > 10 and weightedPercentage <= 20:
+    elif (
+        deviance > 1
+        and deviance <= 4
+        or weightedPercentage > 10
+        and weightedPercentage <= 20
+    ):
         confidence = "Moderate"
-    elif deviance > 4 and deviance <= 6 or weightedPercentage > 20 and weightedPercentage <= 30:
+    elif (
+        deviance > 4
+        and deviance <= 6
+        or weightedPercentage > 20
+        and weightedPercentage <= 30
+    ):
         confidence = "Moderately Low"
-    elif deviance > 6 and deviance <= 7 or weightedPercentage > 30 and weightedPercentage <= 50:
+    elif (
+        deviance > 6
+        and deviance <= 7
+        or weightedPercentage > 30
+        and weightedPercentage <= 50
+    ):
         confidence = "Low"
     else:
         confidence = "Extremely low"
@@ -145,11 +195,13 @@ def predict(datasets):
         + aligndeviances(deviances, predictionlist)
     )
 
+
 def aligndeviances(deviances, dataset):
     output = ""
     for i in range(len(deviances)):
         output += str(dataset[i]) + " +/- " + str(deviances[i]) + "  "
     return output
+
 
 # Utilizes basic functions to create a weather forcast
 def forecast(dataset):
@@ -165,6 +217,7 @@ def forecast(dataset):
         "degrees",
     )
     input("Enter key to return to home: ")
+
 
 # Home screen and user flow control
 def homescreen():
@@ -222,12 +275,14 @@ def homescreen():
     else:
         return
 
+
 # Prints datasets as days (more readable)
 def printasdays(datasets):
     print("Days:")
     for i in range(len(datasets)):
         print("Day " + str(i + 1) + ": ", str(datasets[i]))
     print("\n")
+
 
 # Creates new dataset manually via the user
 def createnewdataset(datasets, values):
@@ -253,6 +308,8 @@ def createnewdataset(datasets, values):
 
 
 print("\n" * 100)
-print("Welcome to an interactive model of a \nPython program a Climatologist would use!")
+print(
+    "Welcome to an interactive model of a \nPython program a Climatologist would use!"
+)
 
 input("Enter key to start: ")
